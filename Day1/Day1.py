2d656f96662742c08d55ef148d3813f0436eaa28
Alex <alexjdickerson1995@gmail.com>
2024-01-12 18:15:36 -0500

    Cleanup

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,28 +1,25 @@
-import json, os
+import os
 
 numDict = {"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7', "eight": '8', "nine": '9',
            '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9' }
 
 #n = number of lines, k = number of keys in numDict, l = number of characters in line
 #O(n*k*l), n and k being static => O(n)
-def main(filePath: str): 
-    lines = open(filePath).readlines()
+def main(filePath: str) -> int:
     sum = 0
-    for l in lines: sum += ParseLine(l) #O(n)
-    
-    print(sum)
+    for l in open(filePath).readlines(): sum += ParseLine(l) #O(n)
+    return sum
 
 def ParseLine(characters: str) -> int:
     digits = ["-1"] * len(characters)
     
-    index1, index2 = -1, -1
+    index1 = index2 = -1
 
     for num in numDict.keys(): #O(k)
         try:
             index = characters.index(num) #O(l)
             lastindex = characters.rfind(num) #O(l)
-            digits[index] = numDict[num]
-            digits[lastindex] = numDict[num]
+            digits[index] = digits[lastindex] = numDict[num]
 
             if(index < index1 or index1 == -1): index1 = index
             if(index > index2): index2 = index
@@ -33,5 +30,4 @@ def ParseLine(characters: str) -> int:
     
     return int(digits[index1] + digits[index2])
 
-dirname = os.path.dirname(__file__)
-main(os.path.join(dirname, "input.txt"))
+print(main(os.path.join(os.path.dirname(__file__), "input.txt")))
